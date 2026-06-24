219671b06f5837d32559e30ce97094d3556eaba0
Axel Cardozo <axelcardozo369@gmail.com>
2026-06-24 15:12:43 -0300

    se implementó funcion de ya con el post y se uso para ver cambios en thunder client

--- src/routes/personajes.routes.js
+++ src/routes/personajes.routes.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 import {
   obtenerPersonajes,
   obtenerPersonajesPorId,
+  crearPersonaje,
 } from "../controllers/personajes.controllers.js";
 const router = Router();
 router.get("/", obtenerPersonajes);
 router.get("/:id", obtenerPersonajesPorId);
+router.post("/", crearPersonaje);
 export default router;
